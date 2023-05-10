3a8bf8e6647f8e7f496dcaa6e1c4774de49777b5
Henrique Buzin <cultura.henrique@gmail.com>
2023-05-10 00:53:14 -0300

    Base

--- server/app.py
+++ server/app.py
@@ -1,10 +1,81 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+nodes = []
+node_id = 0
+clocks = {}  # Vetores de relógio dos nós
+messages = []
 
 @app.route('/')
-def hello():
-    return 'Oi'
+def index():
+    return render_template('index.html')
+
+@app.route('/node', methods=['POST'])
+def create_node():
+    global node_id
+    node_id += 1
+    node = {'node_id': node_id}
+    nodes.append(node)
+    clocks[node_id] = 0  # Inicializa o vetor de relógio do novo nó
+    return jsonify(node), 201
+
+@app.route('/graph', methods=['GET'])
+def get_graph():
+    return jsonify(nodes)
+
+@app.route('/node/<int:node_id>', methods=['DELETE'])
+def remove_node(node_id):
+    global nodes
+    nodes = [node for node in nodes if node['node_id'] != node_id]
+    del clocks[node_id]  # Remove o vetor de relógio do nó removido
+    return jsonify({'message': 'Node removed'}), 200
+
+@app.route('/remove-all-nodes', methods=['DELETE'])
+def remove_all_nodes():
+    global nodes
+    nodes = []
+    clocks.clear()  # Remove todos os vetores de relógio
+    return jsonify({'message': 'All nodes removed'}), 200
+
+@app.route('/send-message', methods=['POST'])
+def send_message():
+    recipient = request.form.get('recipient')
+    message = request.form.get('message')
+
+    sender = int(request.form.get('sender'))  # ID do nó remetente
+    sender_clock = clocks[sender]  # Vetor de relógio do nó remetente
+
+    if recipient == 'broadcast':
+        recipients = [node['node_id'] for node in nodes]
+    else:
+        recipients = [int(recipient)]
+
+    for recipient_id in recipients:
+        recipient_clock = clocks[recipient_id]  # Vetor de relógio do nó destinatário
+
+        # Atualiza o vetor de relógio do remetente antes de enviar a mensagem
+        sender_clock += 1
+        clocks[sender] = sender_clock
+
+        # Cria a mensagem com o vetor de relógio atualizado do remetente
+        message_data = {
+            'sender': sender,
+            'recipient': recipient_id,
+            'message': message,
+            'clock': sender_clock
+        }
+        messages.append(message_data)
+
+        # Atualiza o vetor de relógio do destinatário
+        recipient_clock = max(recipient_clock, sender_clock)
+        clocks[recipient_id] = recipient_clock
+
+    return jsonify(messages), 200
+
+@app.route('/get-messages/<int:node_id>', methods=['GET'])
+def get_messages(node_id):
+    node_messages = [message for message in messages if message['recipient'] == node_id]
+    return jsonify(node_messages), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
