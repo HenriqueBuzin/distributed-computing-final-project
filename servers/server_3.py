f915c60a5090677a57e183f4f5a615c9037350e6
Henrique Buzin <cultura.henrique@gmail.com>
2023-05-13 20:21:32 -0300

    Base

--- servers/server_3.py
+++ servers/server_3.py
@@ -10,8 +10,19 @@ destinations = {
     "server-3": "http://localhost:8003"
 }
 
+port = 8003
+server = "server-3"
+
 # Lista para armazenar as mensagens recebidas
 received_messages = []
+
+# Dicionário para rastrear os relógios lógicos dos servidores
+logical_clocks = {
+    "server-1": 0,
+    "server-2": 0,
+    "server-3": 0
+}
+
 
 @app.route("/send", methods=["POST"])
 def send():
@@ -25,23 +36,43 @@ def send():
     if destination not in destinations:
         return "Erro: destino inválido", 400
 
-    url = f"{destinations[destination]}/message"
-    payload = {"sender": destination, "message": message}
+    # Incrementa o relógio lógico do servidor atual
+    logical_clocks[server] += 1
+
+    # Adiciona o relógio lógico à mensagem
+    payload = {
+        "sender": server,
+        "message": message,
+        "logical_clock": logical_clocks[server]
+    }
+
+    url = f"{destinations[destination]}/receive"
     response = requests.post(url, json=payload)
 
     return "Mensagem enviada com sucesso"
 
-@app.route("/message", methods=["POST"])
-def receive_message():
-    data = request.get_json()
-    sender = data.get("sender")
-    message = data.get("message")
-    received_messages.append({sender: message})
-    return "Mensagem recebida pelo servidor"
 
-@app.route("/receive", methods=["GET"])
+@app.route("/receive", methods=["GET", "POST"])
 def get_received_messages():
-    return jsonify(received_messages)
+    if request.method == "GET":
+        return jsonify(received_messages)
+    elif request.method == "POST":
+        data = request.get_json()
+        sender = data.get("sender")
+        message = data.get("message")
+        logical_clock = data.get("logical_clock")
+
+        # Atualiza o relógio lógico do servidor atual
+        logical_clocks[server] = max(logical_clocks[server], logical_clock) + 1
+
+        received_messages.append({
+            "sender": sender,
+            "message": message,
+            "logical_clock": logical_clock
+        })
+
+        return "Mensagem recebida pelo servidor"
+
 
 @app.route("/broadcast", methods=["POST"])
 def broadcast():
@@ -49,14 +80,17 @@ def broadcast():
     if data and "message" in data:
         message = data["message"]
 
+        # Incrementa o relógio lógico do servidor atual
+        logical_clocks[server] += 1
+
         for destination, url in destinations.items():
-            if destination != "server-3":
-                payload = {"sender": "server-3", "message": message}
-                response = requests.post(f"{url}/message", json=payload)
+            if destination != server:
+                payload = {"sender": server, "message": message, "logical_clock": logical_clocks[server]}
+                response = requests.post(f"{url}/receive", json=payload)
 
-        return "Mensagem enviada para todos os servidores (exceto server-3)"
-
+        return f"Mensagem enviada para todos os servidores (exceto {server})"
     return "Mensagem inválida"
 
+
 if __name__ == "__main__":
-    app.run(host="localhost", port=8003)
+    app.run(host="localhost", port=port)
