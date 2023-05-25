ed6c74dfafb269ec7ad2f24072ebb3d425a07b38
Henrique Buzin <cultura.henrique@gmail.com>
2023-05-25 19:10:39 -0300

    Base

--- servers/server_2.py
+++ servers/server_2.py
@@ -101,7 +101,8 @@ async def send():
 # Rota para receber mensagens
 @app.route("/receive", methods=["POST", "GET"])
 async def receive():
-    
+    await asyncio.sleep(5 * 60)
+
     if request.method == "GET":
         return jsonify(received_messages)
     
