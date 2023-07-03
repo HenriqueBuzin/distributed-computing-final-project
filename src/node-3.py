1efc7866c0f8d0928ae19bfbb38ba6c2e2800c8d
Henrique Buzin <cultura.henrique@gmail.com>
2023-07-03 19:27:21 -0300

    Base

--- src/node-3.py
+++ src/node-3.py
@@ -7,17 +7,13 @@ p2p_start('node-3', 2)
 time.sleep(60)
 
 def process_txt_file():
-    # Obtém o nome do arquivo Python em execução
     file_name = os.path.splitext(__file__)[0] + ".txt"
 
-    # Chama as funções receive() e deliver() para obter as listas de mensagens
     received_messages = receive()
     delivered_messages = deliver()
 
-    # Cria uma lista para armazenar as mensagens formatadas
     formatted_messages = []
 
-    # Formata as mensagens recebidas e adiciona na lista
     for message in received_messages:
         sender = message["sender"]
         destination = message["destination"]
@@ -25,14 +21,12 @@ def process_txt_file():
         formatted_message = f"Mensagem recebida: Remetente: {sender}, Destinatário: {destination}, Mensagem: {text}\n"
         formatted_messages.append(formatted_message)
 
-    # Formata as mensagens entregues e adiciona na lista
     for message in delivered_messages:
         sender = message["sender"]
         text = message["message"]
         formatted_message = f"Mensagem recebida: Remetente: {sender}, Mensagem: {text}\n"
         formatted_messages.append(formatted_message)
 
-    # Escreve as mensagens formatadas no arquivo de saída
     with open(file_name, "w", encoding="utf-8") as output_file:
         output_file.writelines(formatted_messages)
 
